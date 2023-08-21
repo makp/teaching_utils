068050222364f36a56e317f014b3db1744f0e8f6
mak-turing <makmiller@gmail.com>
2023-08-21 11:56:42 -0400

    Turn file into a script

--- make_schedule.py
+++ make_schedule.py
@@ -1,5 +1,9 @@
 from datetime import datetime, timedelta
 import os
+import argparse
+
+
+DATE_FORMAT = '%m-%d-%y'
 
 
 def list_all_dates(first, last, no_class=[], weekdays=[0, 2]):
@@ -17,11 +21,10 @@ def list_all_dates(first, last, no_class=[], weekdays=[0, 2]):
     are marked with an asterisk.
     """
     # Convert str to datetime objects
-    DATE_FORMAT = '%m-%d-%y'
 
     start_date = datetime.strptime(first, DATE_FORMAT)
     end_date = datetime.strptime(last, DATE_FORMAT)
-    no_class_dates = [datetime.strptime(d, DATE_FORMAT) for d in no_class]
+    no_class_dates = {datetime.strptime(d, DATE_FORMAT) for d in no_class}
 
     lst_days = []
 
@@ -50,7 +53,7 @@ def file_name_to_export(lst, output_dir):
     if not lst:
         raise ValueError("Empty list!")
 
-    first_date = datetime.strptime(lst[0], '%m-%d-%y')
+    first_date = datetime.strptime(lst[0], DATE_FORMAT)
     first_month = first_date.strftime('%m')
     year = first_date.strftime('%y')
 
@@ -66,6 +69,28 @@ def file_name_to_export(lst, output_dir):
 
 
 def export_file(lst, output_dir="."):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     file_name = file_name_to_export(lst, output_dir)
     with open(file_name, 'w') as f:
         f.write(', '.join(lst))
+
+
+def main():
+    """
+    Example: python make_schedule.py 7-28-23 12-11-23 --no_class 9-4-23 11-22-23 --output_dir ./schedules
+    """
+    parser = argparse.ArgumentParser(description='Generate teaching schedule.')
+    parser.add_argument('start_date')
+    parser.add_argument('end_date')
+    parser.add_argument('--no_class', nargs='*', default=[])
+    parser.add_argument('--output_dir', default='.')
+    args = parser.parse_args()
+
+    dates = list_all_dates(args.start_date, args.end_date, args.no_class)
+    export_file(dates, args.output_dir)
+
+
+if __name__ == "__main__":
+    main()
