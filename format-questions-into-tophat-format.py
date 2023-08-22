0c7427fca45034edb4018530764082cd68abbd10
mak-turing <makmiller@gmail.com>
2023-08-22 16:09:32 -0400

    Use dataframes as arguments

--- format-questions-into-tophat-format.py
+++ format-questions-into-tophat-format.py
@@ -12,19 +12,21 @@ def get_correct_answer_index(options, correct_answer):
     return options.index(correct_answer) if correct_answer else None
 
 
-def format_question(q, index):
-    options = q['mc_options']
-    if q['question_type'] == 'TF':
+def format_question(row, index):
+    options = row['mc_options']
+    if row['question_type'] == 'TF':
         options = ['True', 'False']
 
-    idx = get_correct_answer_index(options, q['correct_answer'])
-    answers = mark_correct_answer([f"{char}. {option}\n" for char, option in zip(get_option_letters(len(options)), options)], idx)
-    return f"{index}. {q['question']}\n{''.join(answers)}"
+    idx = get_correct_answer_index(options, row['correct_answer'])
+    answers = mark_correct_answer(
+        [f"{char}. {option}\n" for char, option in zip(get_option_letters(len(options)), options)], idx)
+    return f"{index}. {row['question']}\n{''.join(answers)}"
 
 
-def write_formatted_questions(questions, filename="output.txt"):
-    formatted_questions = [format_question(q, idx+1)
-                           for idx, q in enumerate(questions)]
+def write_formatted_questions(df, filename="output.txt"):
+    formatted_questions = [format_question(row, idx+1)
+                           for idx, (_, row) in enumerate(df.iterrows())]
     with open(filename, "w") as file:
         for item in formatted_questions:
             file.write(f"{item}\n")
+
